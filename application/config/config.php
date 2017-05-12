a618946100d995c4790cbb34dda5349f665fccd9
Kiril Kirkov <kiro@inv.bg>
2017-05-12 12:32:55 +0300

    Change codeigniter session name

--- application/config/config.php
+++ application/config/config.php
@@ -399,7 +399,7 @@ $config['encryption_key'] = '';
   |
  */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = 'tyson';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
