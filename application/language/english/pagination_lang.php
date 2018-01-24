1061a8ca3bc99c8122df9e89f1f292cda4273a7b
Kiril Kirkov <kiro_tyson@abv.bg>
2018-01-24 10:43:05 +0200

    Update pagination English file

--- application/language/english/pagination_lang.php
+++ application/language/english/pagination_lang.php
@@ -3,3 +3,5 @@ $lang['first'] = 'First';
 $lang['last'] = 'Last';
 $lang['next'] = 'Next';
 $lang['previous'] = 'Previous';
+$lang['pagination_last_link'] = 'Last';
+$lang['pagination_first_link'] = 'First';
