92531f6590f89b25d2764c0a00a0c3063f668329
Kiril Kirkov <kiro_tyson@abv.bg>
2018-01-24 10:42:38 +0200

    Update pagination lang bulgarian file

--- application/language/bulgarian/pagination_lang.php
+++ application/language/bulgarian/pagination_lang.php
@@ -3,3 +3,6 @@ $lang['first'] = 'Първа';
 $lang['last'] = 'Последна';
 $lang['next'] = 'Следваща';
 $lang['previous'] = 'Предишна';
+$lang['previous'] = 'Предишна';
+$lang['pagination_last_link'] = 'Последна';
+$lang['pagination_first_link'] = 'Първа';
