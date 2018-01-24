3d27ae0b3e6f605e19fdff6356577e74fe414a23
Kiril Kirkov <kiro_tyson@abv.bg>
2018-01-24 10:43:25 +0200

    Update pagination france file

--- application/language/france/pagination_lang.php
+++ application/language/france/pagination_lang.php
@@ -3,3 +3,5 @@ $lang['first'] = 'Premier';
 $lang['last'] = 'Dernier';
 $lang['next'] = 'Prochain';
 $lang['previous'] = 'précédent';
+$lang['pagination_last_link'] = 'Dernier';
+$lang['pagination_first_link'] = 'Premier';
