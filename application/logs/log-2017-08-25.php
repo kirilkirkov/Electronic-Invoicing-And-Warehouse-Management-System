03d19285fd9f85e8ccacbf6606c4d9995fc6d74e
Kiril Kirkov <kirilkirkov@Kirils-MacBook-Pro-2.local>
2026-03-13 08:14:48 +0200

    delete old logs

--- application/logs/log-2017-08-25.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2017-08-25 10:11:01 --> 404 Page Not Found: /index
-ERROR - 2017-08-25 10:11:01 --> 404 Page Not Found: /index
